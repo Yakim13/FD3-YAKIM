5d3ebd8d64348f30f36ca4ac73fa99564f817cf4
Yakim13 <jakim@nsys.by>
2017-12-08 21:11:31 +0300

    final

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -12,27 +12,32 @@ var iShop = React.createClass({
     },
 
     render: function(){
-        var outTableCode=this.props.goods.map(function(v,i,a){
-            return  React.DOM.tr({key:v.uniqueCode},
-                        React.DOM.td(null,v.name),
-                        React.DOM.td(null,
-                            React.DOM.a({href:v.fotoLink,target:"blank"},"ссылка")),
-                        React.DOM.td(null,v.price),
-                        React.DOM.td(null,v.qnt),
-                    )
-            }
-        );
-        return  React.DOM.table(null,
-                    React.DOM.caption(null, "Товары на складе"),
-                    React.DOM.tbody(null,
-                        React.DOM.tr(null, 
-                            React.DOM.th(null,"Товар"),
-                            React.DOM.th(null,"Описание"),
-                            React.DOM.th(null,"Цена"),
-                            React.DOM.th(null,"Количество")),
-                        outTableCode
-                    )
-                );    
+        if (this.props.goods.length>0){
+            var outTableCode=this.props.goods.map(function(v,i,a){
+                return  React.DOM.tr({key:v.uniqueCode},
+                            React.DOM.td(null,v.name),
+                            React.DOM.td(null,
+                                React.DOM.a({href:v.fotoLink,target:"blank"},"ссылка")),
+                            React.DOM.td(null,v.price),
+                            React.DOM.td(null,v.qnt),
+                        )
+                }
+            );
+            return  React.DOM.table(null,
+                        React.DOM.caption(null, "Товары на складе"),
+                        React.DOM.tbody(null,
+                            React.DOM.tr(null, 
+                                React.DOM.th(null,"Товар"),
+                                React.DOM.th(null,"Описание"),
+                                React.DOM.th(null,"Цена"),
+                                React.DOM.th(null,"Количество")),
+                            outTableCode
+                        )
+                    );
+        }                
+        else return React.DOM.div(null,
+                        React.DOM.span(null,'Нет товаров')
+                    );
     },
 
     //array.forEach
