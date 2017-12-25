48cb73a67f43f43b57dd92da7b7404c652c11caa
Yakim13 <jakim@nsys.by>
2017-12-25 23:01:48 +0300

    babel

--- ishop3/main.js
+++ ishop3/main.js
@@ -7,7 +7,7 @@ import iShop3 from './components/iShop3';
 
 
 function initiate(){
-    var product=require('./product.json');
+    let product=require('./product.json');
     ReactDOM.render(
         React.createElement(iShop3,{goods:product}), 
         document.querySelector('section') 
