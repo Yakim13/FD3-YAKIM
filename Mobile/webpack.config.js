5abb18759d6a825a08b94abc4508429903f1346a
Yakim13 <jakim@nsys.by>
2018-01-04 22:10:09 +0300

    start - copy basic work project

---
+++ Mobile/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractCSS = new ExtractTextPlugin({
+    filename: "bundle.css"
+});
+
+module.exports = { 
+    entry: "./main.js", 
+    output:{ 
+        path: __dirname, 
+        filename: "bundle.js"
+    }, 
+    module:{ 
+        rules:[
+            { 
+                test: /\.jsx?$/, // какие файлы обрабатывать
+                exclude: /node_modules/, // какие файлы пропускать
+                use: { loader: "babel-loader" }
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract({
+                    use: ["css-loader"]
+                })
+            }            
+        ] 
+    },
+    plugins: [
+        extractCSS
+    ]
+}
